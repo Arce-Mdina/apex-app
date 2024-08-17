d5001499f9a12ca0a3881f1341431265dcaa084f
Arce-Mdina <nets_05basins@icloud.com>
2024-08-17 20:54:11 +0100

    Tweak

--- src/sections/Domain.jsx
+++ src/sections/Domain.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Domain = () => {
   return (
     <div className="domain">
-      <h1>Why I chose <span style={{ color: "brown" }}>filfoletta.eu</span></h1>
+      <h1>Why I chose <span style={{ color: "brown" }}>Filfoletta.eu</span></h1>
       <div>
         <p>I chose the domain name filfoletta.eu because it has a unique and memorable quality that stands out. Its distinctiveness makes it easy for people to recall, and the playful tone adds a fun element that reflects the personality and energy behind the brand. The name encapsulates both creativity and simplicity, making it the perfect fit.</p>
       </div>
