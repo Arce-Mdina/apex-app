610645e0b1326a8f568b621722a36d0e3938ba9f
arce-mdina <nets_05basins@icloud.com>
2024-10-24 20:11:58 +0100

    Added icon to the top

--- src/components/HeroLeft.jsx
+++ src/components/HeroLeft.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 
 const HeroLeft = ({ icon, title, text, fontSize, svg, color }) => {
     return (
-      <div className="card card-grid-double">
+      <div className="card card-grid-double flex">
         <div className="content">
           <h2 className="card-title">{title}</h2>
           <p className="card-text">{text}</p>
