6076bc5ffcea9e91f634e008ed5425971a75cef3
arce-mdina <nets_05basins@icloud.com>
2024-11-16 14:57:19 +0000

    Update BackButton.jsx

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faCircleChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 const BackButton = () => {
     
@@ -15,7 +15,7 @@ const BackButton = () => {
                 {/* <button>
                     <span style={{ color: "dodgerblue" }}>&#x2190;</span>&nbsp;<span style={{ color: "black" }}>Home</span>
                 </button> */}
-                <Icon icon={faCircleChevronLeft} className="home-btn" onClick={() => navigate('/')} style={{ fontSize: 40 }}/>
+                <Icon icon={faArrowLeft} className="home-btn" onClick={() => navigate('/')} style={{ fontSize: 40 }}/>
             </div>
         </>
     );
