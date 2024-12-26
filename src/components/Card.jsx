cd3bb80e1b4b0079e035a06fd1fadc62cf124edb
arce-mdina <nets_05basins@icloud.com>
2024-12-26 21:05:53 +0000

    Added pro project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
-const Card = ({ heading, desc, classes, color, link, image, heading2 }) => {
+const Card = ({ heading, desc, classes, color, link, image}) => {
 
   const navigate = useNavigate();
 
@@ -10,7 +10,6 @@ const Card = ({ heading, desc, classes, color, link, image, heading2 }) => {
     <div className={`visit-card ${classes}`}>
       <img src={image} style={{ width: "fit", overflow: "hidden", height: "auto", top: 0, right: 0, left: 0, cursor: "pointer" }} onClick={() => window.open(`${link}`)}/>
       <h2 style={{ color, cursor: "pointer" }} onClick={() => window.open(`${link}`)} >{heading}</h2>
-      <h2 style={{ color, cursor: "pointer" }} onClick={() => navigate(`${link}`)} >{heading2}</h2>
       <div>{desc}</div>
     </div>
   )
--- src/sections/Pro.jsx
+++ src/sections/Pro.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 import Card from '../components/Card'
 
+import crypto from '../assets/crypto.webp'
+
 const Pro = () => {
   return (
     <div>
         <h1 className="project-heading"><span style={{ color: "brown" }}>Filfla.eu</span>'s Pro Projects</h1>
         <div className="projects-page">
             <div className="card-grid-double">
-                <Card />
+                <Card 
+                    link="https://crypto.filfla.eu"
+                    heading="Cryptotracker"
+                    desc="Cryptotracker is Filfla.eu's first API-powered application that provides dynamic data from Coincap's API. You can find historical pricing data as well as extra information on each individual crypto"
+                    image={crypto}
+                />
             </div>
         </div>
         
