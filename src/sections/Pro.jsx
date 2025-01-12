1ff677dad9d1b99f6ae9edc28f79ee71014843b2
arce-mdina <nets_05basins@icloud.com>
2025-01-12 15:54:14 +0000

    Added New project: Bible Verse

--- src/sections/Pro.jsx
+++ src/sections/Pro.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 import Card from '../components/Card'
 
 import crypto from '../assets/crypto.webp'
+import bibleVerse from '../assets/bible-verse.webp'
 
 const Pro = () => {
+
   return (
     <div>
         <h1 className="project-heading"><span style={{ color: "brown" }}>Filfla.eu</span>'s Pro Projects</h1>
@@ -15,6 +17,13 @@ const Pro = () => {
                     heading="Cryptotracker"
                     desc="Cryptotracker is Filfla.eu's first API-powered application that provides dynamic data from Coincap's API. You can find historical pricing data as well as extra information on each individual crypto"
                     image={crypto}
+                />
+
+                <Card 
+                  link='https://bible-verse.filfla.eu'
+                  heading="Bible Verse (KJV)"
+                  desc="A Filfla.eu's website giving random Bible verses to its users with the click of a button. This website hopes to bring people to God's Word inspires them to follow the Truth even further."
+                  image={bibleVerse}
                 />
             </div>
         </div>
